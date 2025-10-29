c476efe2120a1895e4e582f178fe74c4456ecb8c
violaine soulas <vhsoulas@gmail.com>
2025-10-29 19:52:53 +0100

    Ajout prestations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,7 @@ class PagesController < ApplicationController
 
   def rdv
   end
+
+  def prestations
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get "quisuisje", to: "pages#quisuisje", as: :quisuisje
   get "rdv", to: "pages#rdv", as: :rdv
+  get "prestations", to: "pages#prestations", as: :prestations
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
